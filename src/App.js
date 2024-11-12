b694c260d969035ee1ef3f96eacba182af97cb7a
KMeanSeo <mskim8055@gachon.ac.kr>
2024-11-12 11:31:40 +0900

    BrowerRouter -> HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import PriorityPage from "./screen/PriorityPage";
 import OptimizedExercisePage from "./screen/OptimizedExercisePage";
